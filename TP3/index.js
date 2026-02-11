0556639c58404ff3eb9d3de8abecabd3e1869b65
Valentin <valentin.fouilloud@etu.unice.fr>
2026-02-11 15:21:29 +0100

    Vercel

--- TP3/index.js
+++ TP3/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const { connect } = require('mongoose');
+const connectDB = require('./connectDB/connectDB');
 dotenv.config();
+const path = require('path');
+
 
 const port = process.env.PORT || 3000;
 const val = "TOTO";
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'front', 'index.html'));
 });
+
+app.use(express.static(path.join(__dirname, 'front')));
+
+connectDB();
 
 app.get('/val', (req, res) => {
   res.send(`You sent the value: ${val}`);
